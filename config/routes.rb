4170a8fa2ff7e44781512826c9dcd041a578dbf0
KyungHo Jung <moltak@gmail.com>
2024-05-04 14:28:07 +0900

    chapter 7 signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,12 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get '/contact', to: 'static_pages#contact'
-  get '/about', to: 'static_pages#about'
   get '/help', to: 'static_pages#help'
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
 
   get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create' # 새로 추가함
+  resources :users
 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  #root "posts#index"
 end
