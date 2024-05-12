718a5a4754bf5069859cf3fb1bd44715862b051a
KyungHo Jung <moltak@gmail.com>
2024-05-12 22:53:46 +0900

    chapter 9

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -30,12 +30,50 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", user_path(@user)
 
-    delete logout_path
+    get logout_path
     assert_not is_logged_in?
     assert_redirected_to root_url
     follow_redirect!
     assert_select "a[href=?]", login_path
-    assert_select "a[href=?]", logout_path,      count: 0
+    assert_select "a[href=?]", logout_path, count: 0
     assert_select "a[href=?]", user_path(@user), count: 0
+  end
+
+  test "login with valid information followed by logout" do
+    get login_path
+    post login_path, params: { session: { email: @user.email, password: "password" } }
+
+    assert is_logged_in?
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template "users/show"
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
+    get logout_path
+    assert_not is_logged_in?
+    assert_redirected_to root_url
+
+    # 2번째 윈도우에서 로그아웃 시도 시뮬레이션
+    get logout_path
+    follow_redirect!
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", logout_path, count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
+  end
+
+  test "login with remembering" do
+    log_in_as(@user, remember_me: "1")
+    assert_equal true, assigns(:user).remember
+  end
+
+  test "login without remembering" do
+    # 쿠키를 저장하고 로그인
+    log_in_as(@user, remember_me: "1")
+    get logout_path
+
+    # 쿠키를 삭제하고 로그인
+    log_in_as(@user, remember_me: "0")
+    assert_empty cookies[:remember_token]
   end
 end
